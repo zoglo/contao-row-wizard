59689db240ff6d5b2705877ca7873d59f315a37f
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-08-30 22:53:21 +0200

    Prefix the controllers

--- assets/column-wizard.js
+++ assets/column-wizard.js
@@ -1,9 +1,8 @@
-import { Application } from '@hotwired/stimulus';
-
-import SimpleColumnWizard from './scripts/column-wizard-controller';
-import Sortable from './scripts/sortable-controller';
 import './styles/column-wizard.pcss';
 
+import { Application } from '@hotwired/stimulus';
+import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers';
+
 window.Stimulus = Application.start();
-Stimulus.register('simple-column-wizard', SimpleColumnWizard);
-Stimulus.register('sortable', Sortable);
+const context = require.context('./scripts', true, /\.js$/);
+Stimulus.load(definitionsFromContext(context));
