e243d8d09b3a2b3e2748ab734708a6d2938c4c23
Sebastian Zoglowek <55794780+zoglo@users.noreply.github.com>
2025-08-31 13:36:16 +0200

    CS

--- src/EventListener/AddAssetsListener.php
+++ src/EventListener/AddAssetsListener.php
@@ -12,16 +12,13 @@ use Symfony\Component\HttpKernel\Event\RequestEvent;
 #[AsEventListener]
 class AddAssetsListener
 {
-    public function __construct(
-        private readonly ScopeMatcher $scopeMatcher,
-        private readonly Packages $package,
-    ) {
+    public function __construct(private readonly ScopeMatcher $scopeMatcher, private readonly Packages $package)
+    {
     }
 
     public function __invoke(RequestEvent $event): void
     {
-        if ($this->scopeMatcher->isBackendMainRequest($event))
-        {
+        if ($this->scopeMatcher->isBackendMainRequest($event)) {
             $GLOBALS['TL_JAVASCRIPT'][] = $this->package->getUrl('simple-column-wizard.js', 'zoglo_simple_column_wizard');
             $GLOBALS['TL_CSS'][] = $this->package->getUrl('simple-column-wizard.css', 'zoglo_simple_column_wizard');
         }
